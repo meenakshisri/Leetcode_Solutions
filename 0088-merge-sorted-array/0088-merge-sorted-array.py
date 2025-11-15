212ec422bae92d7f1fd65e8f46d22487f3fa12ce
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2025-11-15 11:44:59 +0530

    Time: 0 ms (100%), Space: 17.9 MB (43.51%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        
         i, j, k = m-1, n-1, m+n-1
 
         while i>=0 and j>=0:
@@ -18,4 +19,6 @@ class Solution:
             nums1[k] = nums2[j]
             j -= 1
             k -= 1
+     
+
 
