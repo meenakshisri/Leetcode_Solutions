f3665ce76ed27478a8160ed6c7cdaf349889915c
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-01-27 20:06:37 +0530

    Time: 0 ms (100%), Space: 43.7 MB (69.21%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+       int i = m-1;
+       int j = n-1;
+       int k = m+n-1;
+
+       while(j>=0 && i>=0)
+       {
+            if(nums2[j] >= nums1[i])
+            {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            else 
+            {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            k--;
+       }
+
+       //Covering edge case when there are no elements in nums1 but there are elements in nums2
+
+       while(j>=0)
+       {
+        nums1[k] = nums2[j];
+        k--;
+        j--;
+       }
+
+    }
+}
