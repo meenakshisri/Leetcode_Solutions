53928f10db5ddfb6b3c22545924aa16d5aac7f2e
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-01-30 18:28:54 +0530

    Time: 0 ms (100%), Space: 20.7 MB (9.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        numsDict = {}
+        numsDict = {}  #Using dictionary to store record for Key, Value pair
 
         for i, val in enumerate(nums):
             comp = target - val
