df120ac63a2abfbaf21507630c583328304f4df4
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-02-20 13:53:19 +0530

    Time: 17 ms (8.14%), Space: 19.6 MB (76.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        l = 0
-        r = len(s)-1
+        l, r = 0, len(s)-1
 
         while l<r:
-            while l<r and not s[l].isalnum():
+            while l<r and not self.alphanum(s[l]) :
                 l = l+1
-            while l<r and not s[r].isalnum():
-                r = r -1
+
+            while l<r and not self.alphanum(s[r]):
+                r= r-1
+                
             if s[l].lower() != s[r].lower() :
                 return False
-            l += 1
-            r -= 1
+                
+            l,r = l+1, r-1
         return True
 
-
-
+    def alphanum(self, c):
+        return (ord('A') <= ord(c)<=ord('Z') or ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9'))
+        
