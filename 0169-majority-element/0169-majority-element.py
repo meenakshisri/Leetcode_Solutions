847fc785403aae8fb337de41a671a8744bc5b2e2
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-02-24 20:26:16 +0530

    Time: 3 ms (85.8%), Space: 21.4 MB (18.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,28 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        """
-        count = {}
-        majElement, maxCount = 0, 0
         
-        for n in nums :
-            count[n] = count.get(n, 0) + 1
-            if count[n] > maxCount :
-                majElement = n
-                maxCount = max(count[n], maxCount)
-        
-        return majElement
-    """
+        #BOYER-MOORE ALGO
+        res, count = 0, 0
 
-    #Using Boyer Moore Majority Voting Algorithm
+        for n in nums:
+            if count == 0:
+                res = n
+            
+            count += ( 1 if n == res else -1)
 
-        majElement = 0
-        count = 0
-
-        for n in nums :
-            if count == 0 :
-                majElement = n
-            count += (1 if n == majElement else -1)
-        
-        return majElement
-
-
+        return res
