5e5edd4ed717b6976607f6412f27d10e06136fb2
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-04-06 18:52:23 +0530

    Time: 615 ms (66.41%), Space: 22.1 MB (86.32%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,30 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+
+        result = []
         nums.sort()
-        res = []
 
-        for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1]:
+        for i, n in enumerate(nums):
+            if (i>0) and (n == nums[i - 1]) :
                 continue
-            left = i+1
-            right = len(nums)-1
-
-            while left<right:
-                threeSum = nums[i]+nums[left]+nums[right]
-                if threeSum < 0:
-                    left +=1
-                elif threeSum > 0:
-                    right -=1
+            
+            l, r = i+1, len(nums)-1
+            while l<r:
+                if (n + nums[l] + nums[r] > 0):
+                    r = r-1
+                elif (n + nums[l] + nums[r] <0):
+                    l= l+1
                 else:
-                    res.append([nums[i], nums[left], nums[right]])
-                    while left< right and nums[left] == nums[left+1]:
-                        left += 1
-                    while left<right and nums[right] == nums[right-1]:
-                        right -=1
-                
-                    left +=1
-                    right-=1
+                    result.append([n, nums[l], nums[r]])
+                    l = l+1
+                    while nums[l] == nums[l-1] and l<r :
+                        l = l+1
+        
+        return result
 
-        return res    
+
+
+
 
 
 
