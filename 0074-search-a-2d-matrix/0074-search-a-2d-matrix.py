38f57dfbe0800e2d8a4eb005288520d903b62209
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-05-11 19:08:17 +0530

    Time: 0 ms (100%), Space: 19.5 MB (40.31%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,6 +1,9 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
+        if not matrix or not matrix[0]:
+            return False
+
         rows = len(matrix)
         cols = len(matrix[0])
 
