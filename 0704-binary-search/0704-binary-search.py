5db9199312811a9a95406f20fe5f762357b130f8
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-05-30 18:03:11 +0530

    Time: 0 ms (100%), Space: 20.4 MB (95.37%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -4,7 +4,7 @@ class Solution:
         l, r = 0,len(nums)-1
 
         while l<=r:
-            mid = l+(r-l)//2
+            mid = l+(r-l)//2 #or mid = (l+r)//2
 
             if nums[mid] == target:
                 return mid
