5f1124cbd4fc938b59f07f75052131601400844f
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-06-15 02:49:52 +0530

    Time: 0 ms (100%), Space: 9.7 MB (75.59%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    
+      vector<int> freq(26,0);
+
+      for(char ch : s)
+      {
+        freq[ch-'a']++;
+      }  
+      for(char ch : t)
+      {
+        freq[ch-'a']--;
+      }
+
+      for(int n : freq)
+      {
+        if(n != 0)
+        {
+            return false;
+        }
+      }
+      return true;    
+    }
+};
