ce77bd67472d89b0a6a1e48be7443952d2702959
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-06-15 22:05:31 +0530

    Time: 0 ms (100%), Space: 9.9 MB (77.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -7,11 +7,11 @@ public:
 
        while(l<r)
        {
-            while(l<r && !isalnum(s[l]))
+            while(l<r && !isalnum(s[l])) //isalnum is built-in function
                 l++;
             while(l<r && !isalnum(s[r]))
                 r--;
-            if(tolower(s[l]) != tolower(s[r]))
+            if(tolower(s[l]) != tolower(s[r])) //tolower is built-in function
             {
                 return false;
             } 
