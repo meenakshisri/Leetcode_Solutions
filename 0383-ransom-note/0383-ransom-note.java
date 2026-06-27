6aff12cedd28d8f19033cb51697113887dc6290c
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-06-28 00:05:37 +0530

    Time: 3 ms (49.49%), Space: 46.3 MB (67.75%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int freq[] = new int[26];
+
+        for(int i=0; i<magazine.length(); i++)
+        {
+            char ch = magazine.charAt(i);
+            freq[ch-'a']++;
+        }
+
+        for(int i=0; i<ransomNote.length(); i++)
+        {
+            char ch = ransomNote.charAt(i);
+            freq[ch-'a']--;
+        }
+
+        for(int n : freq)
+        {
+            if(n<0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
