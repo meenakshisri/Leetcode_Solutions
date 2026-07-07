9bc7ef8c74687e7f92277c50035bcff679c962ff
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-07-07 18:49:25 +0530

    Time: 3 ms (87.18%), Space: 43.5 MB (15.79%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Deque<Character> st = new ArrayDeque<>();
+
+        for(char ch : s.toCharArray())
+        {
+            if(ch == '(' || ch == '[' || ch == '{')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                if(st.isEmpty()) return false;
+
+                char top = st.pop();
+                if(ch == ')' && top != '(') return false;
+                if(ch == ']' && top != '[') return false;
+                if(ch == '}' && top != '{') return false;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
