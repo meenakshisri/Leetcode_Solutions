15d1200e4cdd4946565d4aef445b973476d95c3c
Meenakshi Srivastava <98895222+meenakshisri@users.noreply.github.com>
2026-08-20 17:57:31 +0530

    Time: 11 ms (77.36%), Space: 19.4 MB (76.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,20 +4,13 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        arr = [0] * 26
-        for ch in s:
-            arr[ord(ch) - ord('a')] += 1
-        
-        for ch in t:
-            arr[ord(ch) - ord('a')] -= 1
-        
-        for n in arr:
-            if n != 0:
+        count = [0] * 26
+        for ch1, ch2 in zip(s, t):
+            count[ord(ch1)-ord('a')] += 1
+            count[ord(ch2)-ord('a')] -= 1
+
+        for num in count:
+            if num != 0:
                 return False
         return True
-
-
-
-        
-
         
